6e296062b0d788886c505843a7ce5169b1226b13
Jon Wolski <jonwolski@gmail.com>
2015-07-03 11:38:40 -0500

    Recursive function to count to ten.
    The second exercise: "Write a function that uses recurstion to count to ten."

--- erlang/day1.erl
+++ erlang/day1.erl
@@ -1,10 +1,16 @@
 -module(day1).
 -include_lib("eunit/include/eunit.hrl").
 -export([words_in_string/1]).
+-export([count_to_ten/0]).
 
 words_in_string( []) -> 0;
 words_in_string( Str ) -> list_length( string:tokens( Str, " " )).
 
 list_length( [] ) -> 0;
 list_length( [_|Tail] ) -> 1 + list_length( Tail ).
+
+count_down_to_zero_from(0) -> 0;
+count_down_to_zero_from(N) -> 1 + count_down_to_zero_from( N-1).
+
+count_to_ten() -> count_down_to_zero_from( 10 ).
 
--- erlang/day1_test.erl
+++ erlang/day1_test.erl
@@ -2,14 +2,16 @@
 -include_lib("eunit/include/eunit.hrl").
 
 words_in_string_test_() ->
-  [?_assert(day1:words_in_string([]) =:= 0),
-   ?_assert(day1:words_in_string("") =:= 0),
-   ?_assert(day1:words_in_string("a") =:= 1),
-   ?_assert(day1:words_in_string("abc") =:= 1)
-   ,?_assert(day1:words_in_string("abc ") =:= 1)
-   ,?_assert(day1:words_in_string("abc def") =:= 2)
-   ,?_assert(day1:words_in_string(" abc def ") =:= 2)
-   ,?_assert(day1:words_in_string(" abc  def ") =:= 2)
-   ,?_assert(day1:words_in_string(" abc  def ghi jkl mnopqrustuvwxyz:a:1 ") =:= 5)
+  [?_assert(day1:words_in_string([]) =:= 0)
+  ,?_assert(day1:words_in_string("") =:= 0)
+  ,?_assert(day1:words_in_string("a") =:= 1)
+  ,?_assert(day1:words_in_string("abc") =:= 1)
+  ,?_assert(day1:words_in_string("abc ") =:= 1)
+  ,?_assert(day1:words_in_string("abc def") =:= 2)
+  ,?_assert(day1:words_in_string(" abc def ") =:= 2)
+  ,?_assert(day1:words_in_string(" abc  def ") =:= 2)
+  ,?_assert(day1:words_in_string(" abc  def ghi jkl mnopqrustuvwxyz:a:1 ") =:= 5)
   ].
+
+count_to_ten_test_() -> ?_assert(day1:count_to_ten() =:= 10).
 
