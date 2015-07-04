ba0ad0554668cf749c894041f9b61ef4e09d3662
Jon Wolski <jonwolski@gmail.com>
2015-07-03 21:24:45 -0500

    Add `line_items_with_subtotals`
    Erlang day 2, exercise 2. Write a list comprehension that takes a list
    of items of the form `{item, quantity, price}` and returns a list of
    items each of the form `{item, total_price}`.

--- erlang/day2.erl
+++ erlang/day2.erl
@@ -1,9 +1,15 @@
 -module(day2).
--export([dictionary_lookup/2]).
+-export([
+  dictionary_lookup/2,
+  line_items_with_subtotals/1]
+).
 
 dictionary_lookup( Dictionary, Key ) ->
   case lists:keyfind( Key, 1, Dictionary ) of
     {_,V} -> V;
     _ -> false
   end.
+
+line_items_with_subtotals( Line_Items ) ->
+  [{Item, Qty * Price} || {Item, Qty, Price} <- Line_Items ].
 
--- erlang/day2_test.erl
+++ erlang/day2_test.erl
@@ -12,3 +12,17 @@ dictionary_lookup_test_() ->
       ], lithium )
     )
   ].
+
+line_items_with_subtotals_test_() ->
+  [?_assertEqual(
+      [],
+      day2:line_items_with_subtotals([])
+    )
+  ,?_assertEqual(
+    [{widget, 100},{gadget, 2400 }],
+     day2:line_items_with_subtotals([
+        {widget, 25, 4},
+        {gadget, 2, 1200}
+      ])
+    )
+  ].
