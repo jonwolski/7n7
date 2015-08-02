d7a11852b3772272fd42e3ef945953d84c31df08
Jon Wolski <jonwolski@gmail.com>
2015-08-01 19:28:17 -0500

    Day 2 exercise 3 "Tree"
    Add `Tree` class.

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -10,5 +10,30 @@ module Day2
   def self.exercise_2
     (1..16).to_a.each_slice(4) { |k| puts k }
   end
+
+  class Tree
+    attr_accessor :children, :node_name
+
+    def visit_all( &block )
+      visit &block
+      children.each {|c| c.visit_all &block}
+    end
+
+    def visit(&block)
+      block.call self
+    end
+
+    def initialize(name, tree_hash )
+      @children = []
+      @node_name = name
+      tree_hash.each { |key, value|
+        @children.push Tree.new(key, value)
+      }
+    end
+
+    def to_s
+      "'#{@node_name}': {#{@children.join ", "}}"
+    end
+  end
 end
 
--- ruby/day2_spec.rb
+++ ruby/day2_spec.rb
@@ -1,7 +1,10 @@
 require './day2'
 
 
-Numbers = <<-OUTPUT
+
+RSpec.describe "Day2" do
+
+  let( :numbers ) { <<-OUTPUT
 1
 2
 3
@@ -19,20 +22,46 @@ Numbers = <<-OUTPUT
 15
 16
 OUTPUT
+}
 
-RSpec.describe "Day2" do
+
   describe "exercise 1" do
     it "outputs four separate times using only #each" do
-      expect { Day2::exercise_1 }.to output( Numbers ).to_stdout
+      expect { Day2::exercise_1 }.to output( numbers ).to_stdout
     end
   end
 
   describe "exercise 2" do
     it "outputs four separate times using only #each" do
-      expect { Day2::exercise_2 }.to output( Numbers ).to_stdout
+      expect { Day2::exercise_2 }.to output( numbers ).to_stdout
     end
   end
 
+
+  describe "exercise 3" do
+
+    let( :family_tree ) {
+      ruby_tree = Day2::Tree.new(
+        "family",
+        {'grandpa' => {
+            'dad' => {
+              'child1' => {},
+              'child2' => {}
+            },
+            'uncle' => {
+              'child3' => {},
+              'child4' => {}
+            }
+          }
+        }
+      )
+    }
+
+    it "accepts a tree literal in the initializer" do
+      visitor = double()
+      expect( visitor ).to receive( :visit ).and_return( *%w{family grandpa dad child1 child2 uncle child3 child4} )
+      family_tree.visit_all {|s| visitor.visit( s )}
+    end
+
+  end
 end
-
-
